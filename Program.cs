dddf9a0b16411b9e2affdfa8536e3ad908fd719c
RokiWielki <Cruck222@interia.pl>
2022-10-30 14:36:45 +0100

    Version 2

--- Program.cs
+++ Program.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
+
 namespace WzorzecFasada
 {
 
     interface IUserService
     {
         void CreateUser(string email);
+        int GetCount();
+        void DeleteUser(string email);
     }
 
     static class EmailNotification
     {
         public static void SendEmail(string to, string subject)
         {
-            Console.WriteLine("Sending an email");
+
         }
     }
 
@@ -27,14 +30,25 @@ namespace WzorzecFasada
 
         public bool IsEmailFree(string email)
         {
-            throw new NotImplementedException();
+            return !users.Contains(email);
             //dopisz implementacje, która zwróci informacje o tym czy email jest dostępny
         }
+        
 
         public void AddUser(string email)
         {
-            throw new NotImplementedException();
+            users.Add(email);
+            Console.WriteLine("Welcome to our service ");
             //dopisz implementacje, która doda użytkownika do listy
+        }
+        public void Delete(string email)
+        {
+            users.Remove(email);
+            Console.WriteLine("Goodbye "+email);
+        }
+        public int Countusers()
+        {
+            return users.Count;
         }
     }
 
@@ -48,7 +62,7 @@ namespace WzorzecFasada
         }
     }
 
-    class UserService : IUserService
+    class UserService : UserRepository, IUserService
     {
         private readonly UserRepository userRepository = new UserRepository();
         public void CreateUser(string email)
@@ -58,11 +72,27 @@ namespace WzorzecFasada
                 throw new ArgumentException("Błędny email");
             }
 
+            if (!userRepository.IsEmailFree(email))
+            {
+                throw new ArgumentException("Zajęty email");
+            } 
+
             // TODO: dodaj sprawdzenie czy email jest wolny, jeśli nie to wyrzuć wyjątek, jeśli tak, kontynuuj wykonywanie funkcji
 
             userRepository.AddUser(email);
             EmailNotification.SendEmail(email, "Welcome to our service");
         }
+        public int GetCount()
+        {
+            return userRepository.Countusers();
+        }
+        public void DeleteUser(string email)
+        {
+            userRepository.Delete(email);
+        } 
+
+
+
     }
 
     class Program
@@ -70,11 +100,16 @@ namespace WzorzecFasada
         static void Main(string[] args)
         {
             IUserService userService = new UserService();
+            Console.WriteLine("Aktualna liczba adresów: "+ userService.GetCount());
             // TODO: wyświetlić liczbę
             userService.CreateUser("someemail@gmail.com");
+            Console.WriteLine("Aktualna liczba adresów: " + userService.GetCount());
             // TODO: wyświetlić liczbę
+            userService.DeleteUser("john.doe @gmail.com");
             // TODO: usunąć użytkownika
+            Console.WriteLine("Aktualna liczba adresów: " + userService.GetCount());
             // TODO: wyświetlić liczbę
+            
         }
     }
 
